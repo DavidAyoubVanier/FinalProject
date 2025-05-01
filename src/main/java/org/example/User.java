46ad8076e4ff59ed96e7727ac5851a991bfba15c
Damage Inc <davidayoub.rwa@gmail.com>
2025-05-01 16:03:29 -0400

    Implementation of User class

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.util.Objects;
+
+public abstract class User {
+    private String id;
+    private String name;
+
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public abstract void viewAvailableVehicles(List<Vehicle> fleet);
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    // Setters and getters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
