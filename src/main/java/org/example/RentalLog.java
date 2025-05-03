52515d38ee3afe2bbd403de337b937c439338eec
Damage Inc <davidayoub.rwa@gmail.com>
2025-05-02 22:06:40 -0400

    Implementation of TODOs in VehicleRentalSystem and RentalLog classes

--- src/main/java/org/example/RentalLog.java
+++ src/main/java/org/example/RentalLog.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class RentalLog {
+    // attributes
+
+
+    // constructor
+
+
+    // toString
+
+
+    // setters, getters
+
+
+    public long getDuration() {
+        // TODO gives the time left for the rental, requires implementation of attributes
+    }
 }
--- src/main/java/org/example/VehicleRentalSystem.java
+++ src/main/java/org/example/VehicleRentalSystem.java
@@ -1,4 +1,28 @@
 package org.example;
 
 public class VehicleRentalSystem {
+    // attributes
+
+
+    public void addVehicle(Vehicle v) {
+        // TODO adds a vehicle to a fleet
+    }
+
+    public void rentVehicle() {
+        // TODO rents a vehicle and writes to the rental logs
+    }
+
+    public void returnVehicle() {
+        // TODO returns a vehicle, checks availability and returns a message
+    }
+
+    public void saveFleetToFile() {
+        // TODO writes information about the fleet to an external file
+    }
+
+    public void loadFleetFromFile() {
+        // TODO reads and prints information about the fleet from a file
+    }
+
+    // getters
 }
