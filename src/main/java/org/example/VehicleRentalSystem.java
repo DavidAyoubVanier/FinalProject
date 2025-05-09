4c38fa668ba4f41cc0ab327d100a81fe73b0ee5c
Damage Inc <davidayoub.rwa@gmail.com>
2025-05-08 23:52:18 -0400

    Implementation of addVehicle, rentVehicle and returnVehicle methods in VehicleRentalSystem class

--- src/main/java/org/example/VehicleRentalSystem.java
+++ src/main/java/org/example/VehicleRentalSystem.java
@@ -1,19 +1,40 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehicleRentalSystem {
-    // attributes
+    private List<Vehicle> fleet = new ArrayList<>();
+    private List<RentalLog> logs = new ArrayList<>();
 
 
     public void addVehicle(Vehicle v) {
-        // TODO adds a vehicle to a fleet
+        fleet.add(v);
     }
 
-    public void rentVehicle() {
-        // TODO rents a vehicle and writes to the rental logs
+    public void rentVehicle(Customer c, String plate, LocalDate rentDate, LocalDate returnDate) {
+        for (Vehicle vehicle : fleet) {
+            if (vehicle.getPlateNumber().equals(plate) && vehicle.isAvailable()) {
+                if (vehicle.rent()) {
+                    logs.add(new RentalLog(vehicle, c, rentDate, returnDate));
+                    System.out.println("Rented: " + vehicle);
+                    return;
+                }
+            }
+        }
+        System.out.println("Vehicle not available.");
     }
 
-    public void returnVehicle() {
-        // TODO returns a vehicle, checks availability and returns a message
+    public void returnVehicle(String plate) {
+        for (Vehicle vehicle : fleet) {
+            if (vehicle.getPlateNumber().equals(plate) && !vehicle.isAvailable()) {
+                vehicle.returnVehicle();
+                System.out.println("Returned: " + vehicle);
+                return;
+            }
+        }
+        System.out.println("vehicle not found or already available.");
     }
 
     public void saveFleetToFile() {
